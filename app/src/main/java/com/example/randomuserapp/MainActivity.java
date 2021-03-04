8ae3a5ae95c8303d017dec82c3d1aa02a444b059
Júlia Braz <63269350+zulia21@users.noreply.github.com>
2021-03-04 16:58:59 -0300

    Atualização do spinner: Inserção de alguns dos idiomas.
    Tela inicial em 95%
    
    TODO: Atributo de cor do triângulo do spinner, trocar para algo mais claro.

--- app/src/main/java/com/example/randomuserapp/MainActivity.java
+++ app/src/main/java/com/example/randomuserapp/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.randomuserapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+
+    Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        spinner = (Spinner) findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.countries, android.R. layout.simple_spinner_dropdown_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+
     }
 }
