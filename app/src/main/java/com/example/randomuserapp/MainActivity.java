26a1b0a03536d630d27b300ad7730ca433bb86b6
Júlia Braz <63269350+zulia21@users.noreply.github.com>
2021-03-08 11:00:20 -0300

    Atualização dos temas: Conserto da exibição em dark e em light theme
    Animações de entrada: Animação de movimento e fade-in/fade-out
    
    TODO: Triângulo do spinner (estético)

--- app/src/main/java/com/example/randomuserapp/MainActivity.java
+++ app/src/main/java/com/example/randomuserapp/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.randomuserapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
     Spinner spinner;
+
+    Button search;
+
+    TextView title;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +32,20 @@ public class MainActivity extends AppCompatActivity {
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.countries, android.R. layout.simple_spinner_dropdown_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
+
+
+        search = (Button) findViewById(R.id.search_on_api_button);
+
+        title = (TextView) findViewById(R.id.random_text);
+
+
+        Animation updown = AnimationUtils.loadAnimation(this, R.anim.up_down);
+        Animation animation1 = AnimationUtils.loadAnimation(this, R.anim.fade);
+
+        search.setAnimation(updown);
+
+        title.setAnimation(animation1);
+
 
 
     }
